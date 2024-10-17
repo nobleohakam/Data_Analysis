e51b232461b5c8bc4091c845144e91cd95d37ae3
Noble Ohakam <63134458+nobleohakam@users.noreply.github.com>
2024-10-17 08:52:31 -0400

    Added file via upload
    this is a simple script to for reading excel files. 
    Must have to call this script in the same place as the file

---
+++ Project.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 20:59:12 2024
+
+@author: nobleohakam
+"""
+#one way to read an excel file with pandas and python
+# import pandas lib as pd
+import pandas as pd
+
+# read by default 1st sheet of an excel file
+
+excel_file = input("write a name of a file in this dir: ")
+print(excel_file)
+
+dataframe1 = pd.read_excel(excel_file)
+
+print(dataframe1)
+
